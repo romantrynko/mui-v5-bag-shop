18ce40bf465b8b5d3bce87cfd6b66cc9d0282846
Roman Trynko <trynkoroman25@gmail.com>
2022-09-21 13:22:30 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Container, ThemeProvider } from '@mui/material';
+import { useEffect } from 'react';
+import theme from './styles/theme';
 
 function App() {
+  useEffect(() => {
+    document.title = 'React MUI - Home';
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container
+        maxWidth='xl'
+        sx={{
+          background: '#fff'
+        }}
+      >
+        {
+          // Appbar
+          // Banner
+          // Promotions
+          // Title
+          // Products
+          // Footer
+          // Searchbox
+          // Appdrawer
+        }
+        <Button variant='contained'>Test</Button>
+      </Container>
+    </ThemeProvider>
   );
 }
 
